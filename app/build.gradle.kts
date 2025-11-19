09b2a15f890aac88ebab82cb637e8f9eb4f1c55c
ivanfernadezm99 <ivanfernadezm99@users.noreply.github.com>
2025-11-19 10:53:43 -0300

    Incrementar versión a 1.3 (versionCode: 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "enlaceschaco.ar"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
         
         multiDexEnabled = true
 
